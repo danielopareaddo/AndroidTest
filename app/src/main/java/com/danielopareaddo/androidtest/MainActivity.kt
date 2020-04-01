83a978389c4d71b6ef58beaf06cd5775f306b634
danielopareaddo <danielopareaddo@yahoo.com>
2020-04-01 19:54:38 +0000

    developer

--- app/src/main/java/com/danielopareaddo/androidtest/MainActivity.kt
+++ app/src/main/java/com/danielopareaddo/androidtest/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var occupation = "developer"
-        var name = "DannyB"
+        var name = "Danny"
 
     }
 }
